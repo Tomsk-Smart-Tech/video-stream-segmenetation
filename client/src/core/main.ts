75a5b910f113edc68c442d0fa9d2ae862929e5d3
Aron <kirillcodminecraft@gmail.com>
2025-10-24 14:47:40 +0700

    feat(virtual-background): Merged ml-module, checkpoint 1

--- client/src/core/main.ts
+++ client/src/core/main.ts
@@ -8,9 +8,9 @@ export async function run() {
   try {
     // Получаем все нужные HTML элементы
     const videoElement = document.getElementById('webcam') as HTMLVideoElement;
-    const outputCanvas = document.getElementById('output') as HTMLCanvasElement;
-    const debugPanel = document.getElementById('debug-panel') as HTMLDivElement; // <-- НАШ НОВЫЙ DIV
-    if (!videoElement || !outputCanvas || !debugPanel) throw new Error('Не найдены video, canvas или debug-panel элементы');
+    const outputCanvas = document.getElementById('output__mask') as HTMLCanvasElement;
+    // const debugPanel = document.getElementById('debug-panel') as HTMLDivElement;
+    // if (!videoElement || !outputCanvas || !debugPanel) throw new Error('Не найдены video, canvas или debug-panel элементы');
 
     await startCamera(videoElement);
     const session = await initializeModel(modelUrl);
@@ -20,31 +20,25 @@ export async function run() {
     const ctx = outputCanvas.getContext('2d');
     if (!ctx) throw new Error('Не удалось получить 2D контекст canvas');
 
-    // --- Переменные для расчета FPS ---
     let frameCount = 0;
     let lastTime = performance.now();
 
     async function gameLoop() {
-      // Получаем результаты замеров из processFrame
       const timings = await processFrame(videoElement, session, ctx);
 
-      // --- Логика расчета и вывода FPS ---
-      const currentTime = performance.now();
-      frameCount++;
-      // Обновляем текст раз в 500 мс, чтобы не так сильно мельтешил
-      if (currentTime - lastTime >= 500) {
-        const fps = (frameCount / (currentTime - lastTime)) * 1000;
-        
-        debugPanel.innerHTML = `
-          FPS: ${fps.toFixed(1)} <br/>
-          Inference: ${timings.inferenceTime.toFixed(2)} ms <br/>
-          Total Frame: ${timings.totalTime.toFixed(2)} ms
-        `;
-
-        frameCount = 0;
-        lastTime = currentTime;
-      }
-      
+      // const currentTime = performance.now();
+      // frameCount++;
+      // if (currentTime - lastTime >= 500) {
+      //   const fps = (frameCount / (currentTime - lastTime)) * 1000;
+      //
+      //   // debugPanel.innerHTML = `
+      //   //   FPS: ${fps.toFixed(1)} <br/>
+      //   //   Inference: ${timings.inferenceTime.toFixed(2)} ms <br/>
+      //   //   Total Frame: ${timings.totalTime.toFixed(2)} ms
+      //   // `;
+      //   frameCount = 0;
+      //   lastTime = currentTime;
+      // }
       requestAnimationFrame(gameLoop);
     }
     
